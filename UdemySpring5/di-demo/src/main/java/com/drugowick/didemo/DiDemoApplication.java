11d92723ce252d421903a469f8270faacdf49e92
Bruno Drugowick Muniz <bruno.drugowick@gmail.com>
2019-03-03 16:21:50 -0300

    Adding code from lesson 41 - Spring Profiles, from springFrameworkGuru repository.

--- UdemySpring5/di-demo/src/main/java/com/drugowick/didemo/DiDemoApplication.java
+++ UdemySpring5/di-demo/src/main/java/com/drugowick/didemo/DiDemoApplication.java
@@ -1,23 +1,4 @@
-package com.drugowick.didemo# Dependency Injection - Spring Context
-
-This application is showing how the Spring Context works.
-
-Notice how I'm getting a context from SpringApplication.run and asking that context to give me a bean from the class MyController.
-
-I'm not running a "new" on the class. I'm asking the context to give me a bean, putting it on a variable "controller" and then calling the hello() method, which successfully prints hello to the console.
-
-Notice also as this application runs normally and exits at the end, unlike a web Spring Boot application would.
-
-By only adding:
-```
-<dependency>
-    <groupId>org.springframework.boot</groupId>
-    <artifactId>spring-boot-starter-web</artifactId>
-</dependency>
-```
-to the pom.xml, I would make it a web application.
-
-It works, I tested it!;
+package com.drugowick.didemo;
 
 import com.drugowick.didemo.controllers.MyController;
 import org.springframework.boot.SpringApplication;
