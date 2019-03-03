633b3f1c65fa57492cd97bebff5e83d98c22252f
Bruno Drugowick Muniz <bruno.drugowick@gmail.com>
2019-03-03 17:32:52 -0300

    Adding a default content.

--- UdemySpring5/from-johns-repository/src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ UdemySpring5/from-johns-repository/src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -4,16 +4,13 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-/**
- * Created by jt on 5/24/17.
- */
 @Service
-@Profile({"en", "default"})
+@Profile("de")
 @Primary
-public class PrimaryGreetingService implements GreetingService {
+public class PrimaryGermanGreetingService implements GreetingService{
 
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting service";
+        return "Nai nai nai nai nai";
     }
 }
